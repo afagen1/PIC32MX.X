430f75c053936b00e30c01c205b7019769deb1b3
TMindenhall <tminden1@gmail.com>
2019-01-31 19:26:57 -0700

    added BNO file

--- main.c
+++ main.c
@@ -21,27 +21,19 @@
 #include "I2C.h"
 #include "ILI9341.h"
 #include "SPI.h"
+#include "FONT.h"
+#include "BNO055.h"
 
-//Globals
-int acc_x, acc_y, acc_z;
-int gyr_x, gyr_y, gyr_z;
-int mag_x, mag_y, mag_z;
-int gravity_x, gravity_y, gravity_z;
-int linear_acc_x, linear_acc_y, linear_acc_z;
 
+void Boot_Message(void);
+void Done_Message(void);
 char buffer_1 [40];
-
-//Globals for filtering
-int last_acc_x, last_acc_y, last_acc_z;
-int last_gyr_x, last_gyr_y, last_gyr_z;
-int last_mag_x, last_mag_y, last_mag_z;
-int last_gravity_x, last_gravity_y, last_gravity_z;
-int last_linear_acc_x, last_linear_acc_y, last_linear_acc_z;
+char c;
 
 //NMEA Strings
 char Baud_Rate_Change_38400 [] = "$PMTK251,38400*27\r\n";
 char NMEA_Update_Rate [] = "$PMTK220,1000*1F\r\n";
-
+int i;
 int main(void) {
     
     //Config Functions
@@ -50,31 +42,35 @@ int main(void) {
     UART_1_Init(9600);
     I2C_1_Init();
     
-    //Null Variables
-    acc_x = acc_y = acc_z = 0;
-    gyr_x = gyr_y = gyr_z = 0;
-    mag_x = mag_y = mag_z = 0;
-    gravity_x = gravity_y = gravity_z = 0;
-    linear_acc_x = linear_acc_y = linear_acc_z = 0;
-    last_acc_x = last_acc_y = last_acc_z = 0;
-    last_gyr_x = last_gyr_y = last_gyr_z = 0;
-    last_mag_x = last_mag_y = last_mag_z = 0;
-    last_gravity_x = last_gravity_y = last_gravity_z = 0;
-    last_linear_acc_x = last_linear_acc_y = last_linear_acc_z = 0;
-    
-    //Boot Message
     TFT_Init();
-    sprintf(buffer_1,"Booting...");
-//    TFT_Text(buffer_1, 20, 20, BLACK, WHITE);
-//    sprintf(buffer_1,"Configuring IMU...");
-//    TFT_Text(buffer_1, 60, 20, BLACK, WHITE);
-//    I2C_1_Write_Byte(BNO_DEVICE, OPR_MODE, 0x00);
-
+    TFT_SetDotSize(1);
+    TFT_SetFont(Courier_New_Bold_20,1);
+    Boot_Message();
+    BNO_Init();
+    BNO_Cal_Routine();
+    
+    TFT_FillScreen(BLACK);
+    Done_Message();
+    
+    c = 0;
+    
     while (1) {
+        c += 1;
         LATBbits.LATB0 ^= 1;
-        int i;
-        for (i = 0; i < 10000000; ++i);
+        if(c == 10){c= 0;}
+        sprintf(buffer_1,"%d",c);
+        TFT_Text(buffer_1, 20, 40, BLACK, WHITE);
+        for (i = 0; i < 1000000; ++i);
     }
+}
+
+void Boot_Message(void){
+    sprintf(buffer_1,"Booting...");
+    TFT_Text(buffer_1, 20, 20, WHITE, BLACK);
+}
+void Done_Message(void){
+    sprintf(buffer_1,"Done...");
+    TFT_Text(buffer_1, 20, 20, BLACK, WHITE);
 }
 /*******************************************************************************
  End of File
